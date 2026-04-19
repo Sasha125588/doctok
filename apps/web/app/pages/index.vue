9c031c82bf80a900fc74725af4f5d47710dce278
sasha125588 <sashavolskiy11@gmail.com>
2026-04-19 22:05:58 +0300

    feat(web): switch index page on viewport for desktop layout (task 1.5)

--- apps/web/app/pages/index.vue
+++ apps/web/app/pages/index.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
+import DesktopShell from '~/components/desktop/DesktopShell.vue'
 import TopicFeed from '~/components/feed/TopicFeed.vue'
+import { useDesktop } from '~/composables/useDesktop'
+
+const isDesktop = useDesktop()
 </script>
 
 <template>
-  <TopicFeed />
+  <DesktopShell v-if="isDesktop" />
+  <TopicFeed v-else />
 </template>
