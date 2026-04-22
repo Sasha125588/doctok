ce476745590fee41887b1505ebdc69d97f3f364f
sasha125588 <sashavolskiy11@gmail.com>
2026-04-22 11:54:17 +0300

    feat(web): add /saved route

---
+++ apps/web/app/pages/saved.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DesktopShell from '~/components/desktop/DesktopShell.vue'
+import SavedPage from '~/components/desktop/SavedPage.vue'
+import { useDesktop } from '~/composables/useDesktop'
+
+const isDesktop = useDesktop()
+</script>
+
+<template>
+  <DesktopShell v-if="isDesktop">
+    <SavedPage />
+  </DesktopShell>
+</template>
